4e2441a43a45d7973d4fec8addfd387a9706ae34
谢良才 <xieliangcai@xieliangcaideMacBook-Pro.local>
2022-02-26 20:00:21 +0800

    第一次作业

---
+++ firstWeek/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/golang/glog"
+	"net/http"
+	"os"
+)
+
+/*
+接收客户端 request，并将 request 中带的 header 写入 response header
+读取当前系统的环境变量中的 VERSION 配置，并写入 response header
+Server 端记录访问日志包括客户端 IP，HTTP 返回码，输出到 server 端的标准输出
+当访问 localhost/healthy 时，应返回 200
+*/
+const ()
+
+func main() {
+	http.HandleFunc("/", handle)
+	http.HandleFunc("/localhost/healthy", handle)
+	http.ListenAndServe(":80", nil)
+
+}
+
+type Handle interface {
+	handle(w http.ResponseWriter, req *http.Request)
+}
+
+func handle(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header{
+		for _, value := range v{
+			w.Header().Set(k, value)
+		}
+	}
+	os.Setenv("VERSION", "1.11")
+	s := os.Getenv("VERSION")
+	w.Header().Set("VERSION", s)
+
+	w.WriteHeader(300)
+
+	glog.Info(req.Host, "200")
+}
+
---
+++ go.mod
@@ -0,0 +1,9 @@
+module CloudNativeHomework
+
+go 1.13
+
+require (
+	github.com/golang/glog v1.0.0
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+)
